9bb2778e06e3ad630654194a29ff1eba7e32c149
Moïse <velez1969@hotmail.es>
2020-11-06 18:44:46 +0100

    second commit

--- views/backend/addBillet.php
+++ views/backend/addBillet.php
@@ -6,19 +6,6 @@
 </div>
 
 <!--Formulaire du billet-->
-<div class="addBillet_container">
-     <form action="index.php?action=addBillet" method="post">
-          <div class="addBillet">
-               <label for="title" class="text-title">Titre</label>
-               <input type="text" class="form-title" name="title" id="title" />
-          </div>
-          <div class="contentBillet">
-               <label for="mytextarea" class="text-content">Contenu</label><br />
-               <textarea class="form-content" name="mytextarea" id="mytextarea"rows="40" cols="95"></textarea>
-          </div>
-               <button type="submit">Envoyer</button>
-     </form>
-</div>
 
 
 
