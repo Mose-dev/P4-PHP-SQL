550c3cab84c26b183317f9b479b53c25eaf80b89
Moïse <velez1969@hotmail.es>
2021-02-19 08:57:35 +0100

    Création de liens dans le controllers frontend

--- controllers/frontend/Page.php
+++ controllers/frontend/Page.php
@@ -1,19 +1,21 @@
 <?php
-//require_once 'models/userManager.php';
 
 class Page
 {
      public function home()
      {
+          $title = "Accueil";
           require('views/frontend/view.php');
      }
      
      public function contact()
      {
+          $title = "Contact";
           require('views/frontend/contact.php');
      }
      public function biographie()
      {
+          $title = "Biographie";
           require('views/frontend/biographie.php');
      }
      
