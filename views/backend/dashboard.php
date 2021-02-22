4ca2d0ae70a218a771a03cbf6304d4560d7e7c9c
Moïse <velez1969@hotmail.es>
2021-02-22 08:02:15 +0100

    Création du dashboard

--- views/backend/dashboard.php
+++ views/backend/dashboard.php
@@ -1,14 +1,108 @@
 <?php require 'views/includes/head.php';?>
 <?php require 'views/includes/menu.php';?>
-<!--Header-->
-     <header>
-          <h1>Billet simple pour l'Alaska</h1>
-     </header>
-     <h2 id="dashboard">Dashboard</h2>
-          <div class="dashboardDiv">
-               <p class="linkContent"><a class="dashboardLink" href="index.php?action=addBillet">Editer un billet</a></p>
-               <p class="linkContent"><a class="dashboardLink" href="index.php?action=deconnexion">Deconnexion</a></p>
+
+<!--Jumbotron-->
+<div class="container text-center">
+     <div class="row">
+          <div class="col mt-2">
+               <div class="jumbotron bg-transparent mt-5">
+                    <h1 class="font-weight-bold text-white mt-3">Dashboard</h1> 
+               </div>
           </div>
+     </div>
+</div>
+<!-- Begin Page Content -->
+<div class="container text-center">
+     <div class="row justify-content-center">
+          <div class="col-xl-3 col-md-6 mb-4">
+               <div class="card shadow h-100 py-2">
+                    <div class="card-body">
+                         <div class="col-auto">
+                         <i class="fas fa-feather-alt fa-2x text-success"></i>
+                         </div>
+                         <div class="font-weight-bold text-uppercase">
+                              <a class="text-success" href="index.php?action=addBillet">Editer un billet</a>
+                         </div>
+                    </div>
+               </div>
+          </div>         
+          <div class="col-xl-3 col-md-6 mb-4">
+               <div class="card shadow h-100 py-2">
+                    <div class="card-body">
+                         <div class="col-auto">
+                         <i class="fas fa-hand-paper fa-2x text-danger"></i>
+                         </div>   
+                         <div class=" font-weight-bold text-uppercase">
+                              <a class="text-danger" href="index.php?action=billetSimple">Retirer un billet</a>
+                         </div>
+                    </div>
+               </div>
+          </div>
+          <div class="col-xl-3 col-md-6 mb-4">
+               <div class="card shadow h-100 py-2">
+                    <div class="card-body">
+                         <div class="col-auto">
+                              <i class="fab fa-accusoft fa-2x text-danger"></i>
+                         </div>   
+                         <div class="font-weight-bold text-uppercase">
+                              <a class="text-danger" href="index.php?action=report">Commentaires signalés</a>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
+     <div class="row justify-content-center">
+          <div class="col-xl-3 col-md-6 mb-4">
+               <div class="card shadow h-100 py-2">
+                    <div class="card-body">
+                         <div class="col-auto">
+                              <i class="fas fa-paper-plane fa-2x text-primary"></i>
+                         </div>
+                         <div class="font-weight-bold text-uppercase">
+                              <a class="text-primary" href="index.php?action=getmessages">Messages</a>
+                         </div>
+                    </div>
+               </div>
+          </div>
+          <div class="col-xl-3 col-md-6 mb-4">
+               <div class="card shadow h-100 py-2">
+                    <div class="card-body">
+                         <div class="col-auto">
+                              <i class="fas fa-users fa-2x text-primary"></i>
+                         </div>
+                         <div class="font-weight-bold text-uppercase">
+                              <a class="text-primary" href="index.php?action=getUsers">Adhérants</a> 
+                         </div>
+                    </div>
+               </div>
+          </div>
+          <div class="col-xl-3 col-md-6 mb-4">
+               <div class="card shadow h-100 py-2">
+                    <div class="card-body">
+                         <div class="col-auto">
+                         <i class="fas fa-bed fa-2x text-danger"></i>
+                         </div>
+                         <div class="font-weight-bold text-uppercase">
+                              <a class="text-danger"href="index.php?action=deconnexion">Deconnexion</a>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
+</div>
+
+                    
+
+               
+              
+               
+      
+               
+          
+          
+          
+          
+         
 
   
 
