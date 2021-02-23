9b095a2053069a4fc975a76d98bcfdc1f787f186
Moïse <velez1969@hotmail.es>
2021-02-23 07:54:58 +0100

    Création du footer

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,35 +1,38 @@
-     <footer id="end">
-          
-               <div class="menu_end">
-               <h2>PLAN DU SITE</h2>
-                    <ul>
-                         <li><a href="index.php?action=home">Accueil</a></li>
-                         <li><a href="index.php?action=biographie">Biographie</a></li>
-                         <li><a href="index.php?action=billetSimple">Billet simple</a></li>
-                         <li><a href="index.php?action=contact">Contact</a></li>
-                    </ul>
+<footer>
+     <div class="container">
+          <div class="row">
+               <div class="mb-5 col-8 offset-2 col-md-4 offset-md-0">
+                    <h5 class="text-uppercase text-white">Plan du site</h5>
+                    <div>
+                         <p><a href="index.php?action=home" class="text-white">Accueil</a></p>
+                         <p><a href="index.php?action=biographie" class="text-white">Biographie</a></p>
+                         <p><a href="index.php?action=billetSimple" class="text-white">Billet Simple</a></p>
+                         <p><a href="index.php?action=contact" class="text-white">Contact</a></p>
+                    </div>
                </div>
-          
-               <div>
-               <h2>REJOIGNEZ MOI SUR</h2>
-                    <ul>
-                         <li><a href="https://twitter.com/">Twitter</a></li>
-                         <li><a href="https://facebook.com">Facebook</a></li>
-                         <li><a href="https://www.instagram.com/?hl=fr">Instagram</a></li>
-                    </ul>
+               <div class="mb-5 col-8 offset-2 col-md-4 offset-md-0">
+                    <h5 class="text-uppercase text-white">Me suivre sur</h5>
+                    <div>
+                         <p><a href="https://facebook.com" title="Page Facebook du développeur du site" class="text-white"><span class="fab fa-facebook-square logo"></span> Facebook</a></p>
+                         <p><a href="https://twitter.com/" title="Compte Twitter du développeur du site" class="text-white"><span class="fab fa-twitter-square logo"></span> Twitter</a></p>
+                         <p><a href="https://www.instagram.com/?hl=fr" class="text-white"><span class="fab fa-instagram-square logo"></span> Instagram</a></p>
+                    </div>
                </div>
-        
-               <div>
-               <h2>ADMIN</h2>
-                    <?php if(isset($_SESSION['user']) || isset($_SESSION['admin'])  ):?>
-                         <a href="index.php?action=deconnexion">Deconnexion</a>
+               <div class="text-center mb-5 col-8 offset-2 text-md-left col-md-4 offset-md-0">
+                    <h5 class="text-uppercase text-white">Admin</h5>
+                    <?php if(isset($_SESSION['admin'])): ?>
+                         <a href="index.php?action=deconnexion"><span style="font-size: 1.5em; color: Green;"><i class="fas fa-lock-open"></i></span></a></li>
                     <?php else:?>
-                         <a href="index.php?action=connexion">Se connecter</a>
+                         <a href="index.php?action=connexion"><span style="font-size: 1.5em; color: Red;"><i class="fas fa-lock"></i></span></a></li>
                     <?php endif;?>
                </div>
-
-
-     </footer>
-</div>
+          </div>
+     </div>
+</footer>
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>     
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="assets/JS/flashExplode.js"></script>
 </body>
 </html>
