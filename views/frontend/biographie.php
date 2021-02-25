48cfd0d808936ee86441af4595ea643c6feec308
Moïse <velez1969@hotmail.es>
2021-02-25 17:33:54 +0100

    Création de la page biographie

--- views/frontend/biographie.php
+++ views/frontend/biographie.php
@@ -1,52 +1,52 @@
-<?php require 'views/includes/head.php';?>
-<?php require 'views/includes/menu.php';?>
+<?php require 'views/includes/head.php'; ?>
+<?php require 'views/includes/menu.php'; ?>
 
-<!--Header-->
-     <header id="biographie">
-          
-          <h1>Jean Forteroche</h1>
-          
-     </header>
-<!--Sections-->
-<section>
-     <h2 class="introduction"> Romancier de renom</h2>
-</section>
-     <section  id="biographie2" class="section">
-          <figure>
-               <img src="assets/images/ecrivain.jpg" alt="Un écrivain" />
-          </figure>
-          <div class="projet">
-               <h1>Comment présenter Jean Forteroche ?</h1>
-               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat corporis numquam est nostrum,<br />
-                  at nesciunt odio necessitatibus totam. Quo adipisci sunt officia iure eum accusantium maxime e<br />
-                  officiis sint? Perspiciatis, dolorem. Lorem ipsum dolor sit amet consectetur adipisicing elit.<br />
-                  Quaerat corporis numquam est nostrum, at nesciunt odio necessitatibus totam. Quo adipisci su<br />
-                  officia iure eum accusantium maxime officiis sint? Perspiciatis, dolorem. Lorem ipsum dolor<br />
-                  sit amet consectetur adipisicing elit. Quaerat corporis numquam est nostrum,sit ametsit ame<br />
-                  at nesciunt odio necessitatibus totam. Quo adipisci sunt officia iure eum accusantium maxim<br />
-                  officiis sint? Perspiciatis, dolorem.
-               </p>
-               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat corporis numquam est nostrum,<br />
-                  at nesciunt odio necessitatibus totam. Quo adipisci sunt officia iure eum accusantium maxime e<br />
-                  officiis sint? Perspiciatis, dolorem. Lorem ipsum dolor sit amet consectetur adipisicing elit.<br />
-                  Quaerat corporis numquam est nostrum, at nesciunt odio necessitatibus totam. Quo adipisci su<br />
-                  officia iure eum accusantium maxime officiis sint? Perspiciatis, dolorem. Lorem ipsum dolor<br />
-                  sit amet consectetur adipisicing elit. Quaerat corporis numquam est nostrum,sit ametsit ame<br />
-                  at nesciunt odio necessitatibus totam. Quo adipisci sunt officia iure eum accusantium maxim<br />
-                  officiis sint? Perspiciatis, dolorem.
-               </p>
-               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat corporis numquam est nostrum,<br />
-                  at nesciunt odio necessitatibus totam. Quo adipisci sunt officia iure eum accusantium maxime e<br />
-                  officiis sint? Perspiciatis, dolorem. Lorem ipsum dolor sit amet consectetur adipisicing elit.<br />
-                  Quaerat corporis numquam est nostrum, at nesciunt odio necessitatibus totam. Quo adipisci su<br />
-                  officia iure eum accusantium maxime officiis sint? Perspiciatis, dolorem. Lorem ipsum dolor<br />
-                  sit amet consectetur adipisicing elit. Quaerat corporis numquam est nostrum,sit ametsit ame<br />
-                  at nesciunt odio necessitatibus totam. Quo adipisci sunt officia iure eum accusantium maxim<br />
-                  officiis sint? Perspiciatis, dolorem.
-               </p>
+<!--Jumbotron-->
+<div class="container text-center">
+     <div class="row">
+               <div class="col mt-2">
+                    <div class="jumbotron bg-transparent mt-5 pb-1">
+                         <h1 class="font-weight-bold text-white mt-3">
+                              Jean Forteroche
+                         </h1>
+                         <h2 class="mt-5 font-weight-bold"> 
+                              Romancier de renom 
+                         </h2> 
+                    </div>
+               </div>
+     </div>     
+</div>
+<!--Container de la page Biographie-->
+<div class="container p-4" id="background">
+     <div class="row" id="ancre">
+          <div class="col col-12 col-lg-4">
+               <div class="card">
+                    <img class="card-img-top" src="assets/images/ecrivain.jpg" alt="Un écrivain" />
+               </div>
+          </div> 
+          <div class="col col-12 col-lg-8">
+               <div class="card border shadow">
+                    <div class="card-body">
+                       <h3 class="card-title font-weight-bold">Comment présenter Jean Forteroche ?</h3>
+                         <p class="card-text Bolder weight text"> Né le 18 septembre 1950 à Paris, est un romancier, journaliste et un interprète français. 
+                            Il a acquis une renommée internationale avec la publication de L'Affaire M21, vendu à 85 millions d'exemplaires. En ajoutant ses autres
+                            ouvrages, ce sont 350 millions d'exemplaires vendus à travers le monde. Ses romans ont été traduits dans 81 langues du monde entier.
+                         </p>
+                         <p>Introverti et rebelle, il s'oppose au chemin tracé par ses parents. Son père, désemparé par cet enfant difficile, le fait interner dans 
+                              un hôpital psychiatrique alors qu’il n’avait que dix-sept ans. Il s'en est échappé trois fois avant d’être relâché à l'âge de 20 ans. 
+                              Jean Forteroche dit à ce sujet « Ils n'ont pas fait ça pour me faire souffrir... mais ils ne savaient pas quoi faire. Ils n'ont pas fait 
+                              ça pour me détruire, ils ont fait ça pour me sauver. » Bien des années plus tard, l’écrivain puisera dans cette expérience pénible le matériau de son roman
+                         </p>
+                         <p>
+                              Pour faire plaisir à ses parents, Jean décide de suivre des études de droit et met de côté son rêve de devenir écrivain. Mais il abandonne tout un an plus tard.
+                              Les années 1970 voient l’explosion internationale du mouvement hippie. Paulo y souscrit, ainsi qu’à tous ses excès. À l'âge de 23 ans, il abandonne sa ville natale pour 
+                              voyager à travers le Mexique, le Pérou, la Bolivie et le Chili, ainsi qu'à travers l'Europe et l'Afrique du Nord. Deux ans plus tard, il revient en France et commence à 
+                              composer des paroles de chansons populaires, travaillant avec des musiciens tels que Paul Seixas. Leur association est un succès, et leur collaboration contribue à changer 
+                              le visage de la scène rock. Jean Forteroche s'est réconcilié avec la confession catholique en rencontrant sa femme Christina Oiticica, artiste peintre.
+                         </p>
+                    </div>
+               </div>
           </div>
-     
-     </section>
-     <?php
-          require 'views/includes/footer.php';
-     ?>
+     </div>
+</div>
+<?php require 'views/includes/footer.php'; ?>
