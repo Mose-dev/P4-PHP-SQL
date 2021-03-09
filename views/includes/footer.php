841a87af8e55c01edd0deed89d7d5b25f3285b0b
Moïse <velez1969@hotmail.es>
2021-03-09 19:33:14 +0100

    Ajout d'un bouton de retour vers le haut de page

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,4 +1,7 @@
 <footer>
+<div class="buttonCenter">
+<button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fas fa-angle-double-up"></i></button>
+</div>
      <div class="container">
           <div class="row">
                <div class="mb-5 col-8 offset-2 col-md-4 offset-md-0">
@@ -28,11 +31,13 @@
                </div>
           </div>
      </div>
+     <?php unset($_SESSION["error"]); ?>  
 </footer>
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>     
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script src="assets/JS/flashExplode.js"></script>
+<script src="assets/JS/button.js"></script>
 </body>
 </html>
